b148d2eaa8956bc1905332269cbd65a11f945c8d
jackrichter <jackrichter1@gmail.com>
2026-02-24 16:04:01 +0100

    Starter project setup.

---
+++ src/test/java/guru/springframework/spring7resttemplate/Spring7ResttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7ResttemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
