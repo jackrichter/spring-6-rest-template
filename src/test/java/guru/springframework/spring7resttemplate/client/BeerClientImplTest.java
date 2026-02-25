fe57c684bae128bfea872d536f46ffc66c5f140e
jackrichter <jackrichter1@gmail.com>
2026-02-25 17:10:44 +0100

    Setting up the infrastructure for the Rest Client app.

---
+++ src/test/java/guru/springframework/spring7resttemplate/client/BeerClientImplTest.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring7resttemplate.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerClientImplTest {
+
+    @Test
+    void listBeers() {
+    }
+}
