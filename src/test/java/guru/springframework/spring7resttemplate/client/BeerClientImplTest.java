14edcaa9ecf34eb465e148f4941e2c3f88acf0eb
jackrichter <jackrichter1@gmail.com>
2026-03-02 04:59:23 +0100

    How to pass URL parameters (in this case beerId) to the RestClient.

--- src/test/java/guru/springframework/spring7resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring7resttemplate/client/BeerClientImplTest.java
@@ -1,14 +1,30 @@
 package guru.springframework.spring7resttemplate.client;
 
+import guru.springframework.spring7resttemplate.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BeerClientImplTest {
 
     @Autowired
     BeerClientImpl beerClient;
+
+    @Test
+    void getBeersById() {
+        Page<BeerDTO> beerDTOs = beerClient.listBeers();
+        BeerDTO beerDTO = beerDTOs.getContent().get(0);
+
+        BeerDTO byId = beerClient.getBeerById(beerDTO.getId());
+
+        assertNotNull(byId);
+    }
 
     @Test
     void listBeersNoBeerName() {
